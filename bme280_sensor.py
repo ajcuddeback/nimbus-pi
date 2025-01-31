2bd135d4bcb480a4bc295075addf7c3199f6c26e
austin-cuddeback <acuddeback@sohosquared.com>
2025-01-31 13:42:47 -0500

    Changing C to F

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -3,7 +3,7 @@ import smbus2
 from time import sleep
 
 port = 1
-address = 0x76 # Adafruit BME280 address. Other BME280s may be different
+address = 0x76 # BME280 Address
 bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus,address)
@@ -12,6 +12,6 @@ while True:
     bme280_data = bme280.sample(bus,address)
     humidity  = bme280_data.humidity
     pressure  = bme280_data.pressure
-    ambient_temperature = bme280_data.temperature
+    ambient_temperature = (9/5) * bme280_data.temperature + 32
     print(humidity, pressure, ambient_temperature)
     sleep(1)
