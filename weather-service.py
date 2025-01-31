a93abe584e78db537d64ebc2629c1fc1dee012bf
austin-cuddeback <acuddeback@sohosquared.com>
2025-01-31 13:52:35 -0500

    Calling on 1 seconds cadence

--- weather-service.py
+++ weather-service.py
@@ -1,5 +1,10 @@
 import bme280_sensor
 
-weather_data = bme280_sensor.get_all_data()
 
-print(weather_data)
+
+while True:
+    weather_data = bme280_sensor.get_all_data()
+
+    print(weather_data)
+
+    sleep(1)
