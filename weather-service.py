93b20552182c05b9087a64fc03e7b9ef7187b831
ajcuddeback <ajcuddeback@gmail.com>
2025-03-09 11:35:01 -0400

    feat: Adding very basic publish logic using Paho

--- weather-service.py
+++ weather-service.py
@@ -1,10 +1,34 @@
 import bme280_sensor
 from time import sleep
+import paho.mqtt.publish as publish
+from paho.mqtt.enums import MQTTProtocolVersion
+import logging
 
-
+logging.basicConfig(level=logging.INFO)
 
 while True:
-    weather_data = bme280_sensor.get_all_data()
+    try:
+        weather_data = bme280_sensor.get_all_data()
+    except Exception as sensor_error:
+        logging.error(f"Failed to retrieve sensor data! {sensor_error}")
+
+    data = {
+        "temp": weather_data.temperature,
+        "temp_format": "C",
+        "humidity": weather_data.humidty,
+        "humidity_format": "%",
+        "pressure": weather_data.pressure,
+        "pressure_format": "hPa",
+        "timestamp": weather_data.timestmap
+    }
+
+    try:
+        publish.multiple(data, hostname="localhost", protocol=MQTTProtocolVersion.MQTTv5)
+    except Exception as mqtt_error:
+        logging.error(f"MQTT publish failed: {mqtt_error}")
+    else:
+        logging.info("Data published successfully.")
+
     print(weather_data)
     print('-------------------------------------------------')
     sleep(1)
