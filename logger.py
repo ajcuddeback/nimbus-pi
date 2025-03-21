3d6bafe20a52ea0a91c7aa39c8f054bea53f7f17
austin-cuddeback <acuddeback@sohosquared.com>
2025-03-21 18:33:59 -0400

    fix: Fixing logger

--- logger.py
+++ logger.py
@@ -3,20 +3,19 @@ import logging
 class Logger:
     _instance = None
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, location, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super(Logger, cls).__new__(cls)
-            cls._instance._init_logger()
+            cls._instance._init_logger(location)
         return cls._instance
 
-    def __init__(self, location):
+    def _init_logger(self, location):
         self.location = location
 
-    def _init_logger(self):
         self.log = logging.getLogger(self.location)
         self.log.setLevel(logging.INFO)
 
-        if not self.log.hasHandlers():
+        if not self.log.handlers:
             console_handler = logging.StreamHandler()
             file_handler = logging.FileHandler('main.log')
 
