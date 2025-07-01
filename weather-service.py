e3ce9fb0b7d3f1fa7b5b90a59102fa7c7d5bc964
ajcuddeback <ajcuddeback@gmail.com>
2025-06-30 20:19:59 -0400

    feat: Adding some code improvements for fetching station id

--- weather-service.py
+++ weather-service.py
@@ -8,12 +8,16 @@ import os
 load_dotenv()
 logger_instance = Logger(location=os.getenv('STATION_NAME'))
 
+required_envs = ['STATION_NAME', 'STATION_LON', 'STATION_LAT', 'STATION_CITY', 'STATION_STATE', 'WEATHER_POLLING_RATE']
+for var in required_envs:
+    if not os.getenv(var):
+        raise EnvironmentError(f"Missing environment variable: {var}")
+
 mqtt_client_instance = MQTTClient(host="localhost", port=1883)
 
 has_station_id = False
 
 weather_station_data = {
-    "coordinates": { "long": os.getenv('STATION_LONG'), "lat": os.getenv('STATION_LAT') },
     "lon": os.getenv("STATION_LON"),
     "lat": os.getenv("STATION_LAT"),
     "city": os.getenv("STATION_CITY"),
@@ -23,37 +27,31 @@ weather_station_data = {
 logger_instance.log.info("Sending Station Id Request")
 mqtt_client_instance.publish("stationId/request", weather_station_data)
 
-while has_station_id == False:
-    if mqtt_client_instance._station_id != "":
-        has_station_id = False
-    else: 
-        has_station_id = True
-    
-    sleep(int(os.getenv('STATION_ID_POLLING_RATE')))    
+weather_rate = int(os.getenv("WEATHER_POLLING_RATE"))
 
-while has_station_id == True:
+while True:
     if mqtt_client_instance._station_id == "":
-        has_station_id = False
-        logger_instance.log.fatal("Station id doesn't exist!")
-
+        logger_instance.log.warning("Station ID not populated yet")
+        sleep(2)
+        continue
 
     try:
         weather_data = bme280_sensor.get_all_data()
+        data = {
+            "temp": round(weather_data.temperature, 2),
+            "temp_format": "C",
+            "hum": round(weather_data.humidity, 2),
+            "pr": round(weather_data.pressure, 2),
+            "pr_format": "hPa",
+            "timestamp": round(weather_data.timestamp.timestamp()),
+            "stationId": mqtt_client_instance._station_id
+        }
+
+        mqtt_client_instance.publish("weather/data", data)
+        logger_instance.log.info(weather_data)
+        logger_instance.log.info('-------------------------------------------------')
+
     except Exception as sensor_error:
-        logger_instance.error(f"Failed to retrieve sensor data! {sensor_error}")
+        logger_instance.log.error(f"Failed to retrieve sensor data! {sensor_error}")
 
-    data = {
-        "temp": round(weather_data.temperature, 2),
-        "temp_format": "C",
-        "hum": round(weather_data.humidity, 2),
-        "pr": round(weather_data.pressure, 2),
-        "pr_format": "hPa",
-        "timestamp": round(weather_data.timestamp.timestamp()),
-        "stationId": mqtt_client_instance._station_id
-    }
-
-    mqtt_client_instance.publish("weather/data", data)
-
-    logger_instance.log.info(weather_data)
-    logger_instance.log.info('-------------------------------------------------')
-    sleep(int(os.getenv('WEATHER_POLLING_RATE')))
+    sleep(weather_rate)
