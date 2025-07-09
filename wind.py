fb334ee886b7887809eef360a236d047cc10cef2
ajcuddeback <ajcuddeback@gmail.com>
2025-07-09 19:21:40 -0400

    feat: Adding wind speed sensor

--- wind.py
+++ wind.py
@@ -1,11 +1,14 @@
 from gpiozero import Button
+from signal import pause  # <- This keeps the script running
 
 wind_speed_sensor = Button(5)
 wind_count = 0
 
 def spin():
     global wind_count
-    wind_count = wind_count + 1
-    print("spin" + str(wind_count))
+    wind_count += 1
+    print("spin " + str(wind_count))
 
 wind_speed_sensor.when_pressed = spin
+
+pause()
