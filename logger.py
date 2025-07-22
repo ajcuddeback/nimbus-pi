81fe8bb4083726252feffdcff280ab550d6fdec6
ajcuddeback <ajcuddeback@gmail.com>
2025-07-22 19:55:12 -0400

    feat: Adding env variable for logging level

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 class Logger:
     _instance = None
@@ -13,7 +14,7 @@ class Logger:
         self.location = location
 
         self.log = logging.getLogger(self.location)
-        self.log.setLevel(logging.INFO)
+        self.log.setLevel(self._get_log_level())
 
         if not self.log.handlers:
             console_handler = logging.StreamHandler()
@@ -22,7 +23,7 @@ class Logger:
             formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s', 
                                           datefmt='%m/%d/%Y %I:%M:%S %p')
 
-            console_handler.setLevel(logging.INFO)
+            console_handler.setLevel(self._get_log_level())
             console_handler.setFormatter(formatter)
             file_handler.setLevel(logging.ERROR)
             file_handler.setFormatter(formatter)
@@ -30,4 +31,18 @@ class Logger:
             self.log.addHandler(file_handler)
             self.log.addHandler(console_handler)
 
-
+    def _get_log_level(self):
+        log_level = os.getenv('LOG_LEVEL')
+        match log_level:
+            case "debug":
+                return logging.DEBUG
+            case "info":
+                return logging.INFO
+            case "warn":
+                return logging.WARN
+            case "error":
+                return logging.ERROR
+            case "critical":
+                return logging.CRITICAL
+            case _:
+                return logging.WARN
