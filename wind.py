9d1c36af80d5c49778e26a8df0412f1044a3f6ff
ajcuddeback <ajcuddeback@gmail.com>
2025-07-26 22:43:49 -0400

    feat: Adding better wind speed collection

--- wind.py
+++ wind.py
@@ -11,9 +11,10 @@ logger_instance = Logger(location=os.getenv('STATION_NAME'))
 class WindSpeed:
     _instance = None
     _lock = threading.Lock()
+    _wind_speed_lock = threading.Lock()
     _wind_speed_sensor = Button(5)
     _wind_count = 0
-    curent_wind_speed = 0
+    _wind_speeds = []
     _mph_per_switch = 1.492
     _sample_rate = 5
 
@@ -30,7 +31,7 @@ class WindSpeed:
             super().__init__()
             self.running = False
             self._initialized = True
-            self._wind_speed_sensor.when_pressed = self.spin
+            self._wind_speed_sensor.when_pressed = self._on_spin
 
 
     def run(self):
@@ -39,21 +40,32 @@ class WindSpeed:
             while self.running:
                 time.sleep(self._sample_rate)
                 triggers_per_second = self._wind_count / self._sample_rate
-                self.curent_wind_speed = triggers_per_second * self._mph_per_switch
+                self._wind_speeds.append(triggers_per_second * self._mph_per_switch)
                 self._wind_count = 0
         except Exception as e:
             logger_instance.log.error(f"WindSpeed thread error: {e}")
         finally:
             logger_instance.log.info("WindSpeed thread exiting cleanly")
+
+    def get_wind_speed_average(self):
+        if not self._wind_speeds:
+            return 0.0
+
+        with self._wind_speed_lock:
+            avg = sum(self._wind_speeds) / len(self._wind_speeds)
+            self._wind_speeds.clear()
+        return avg
        
 
     def stop(self):
         logger_instance.log.info("Shutting wind speed thread down")
         self.running = False
         self._wind_speed_sensor.when_pressed = None
-        self._wind_count = 0
-        self.curent_wind_speed = 0
+        with self._wind_speed_lock:
+            self._wind_count = 0
+            self._wind_speeds.clear()
     
-    def spin(self):
-        self._wind_count += 1
+    def _on_spin(self):
+        with self._wind_speed_lock:
+            self._wind_count += 1
         # logger_instance.log.info("spin " + str(self._wind_count))
